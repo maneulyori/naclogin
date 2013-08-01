2487ee140ea7a046c3c2b55891f68f4ebbd2a2c6
zhongfu <me@znx.cc>
2013-08-01 10:43:33 +0800

    added tag to syslogging

--- cca.sh
+++ cca.sh
@@ -16,7 +16,7 @@ password=""
 provider="LDAP"
 
 ## Script
-logger -i -s "naclogin: starting authentication"
+logger -t "naclogin" -i -s "starting authentication"
 # get address for authentication
 authredurl=$(curl -k -s http://www.google.com | grep -o URL=.*\</head | sed -e 's#URL=##' -e "s#'></head##")
 if [ "$authredurl" != "" ]; then
@@ -38,21 +38,21 @@ if [ "$authredurl" != "" ]; then
 	uri="http://www.google.com/"
 	reqfrom="perfigo_login.jsp"
 	cm=""
-	remove_old=1
+	remove_old="1"
 
-	fullstring="username=$username&password=$password&provider=$provider&guestUserNameLabel=$guestusernamelabel&guestPasswordLabel=$guestpasswordlabel&passwordLabel=$passwordlabel&userNameLabel=$usernamelabel&registerGuest=$registerguest&compact=$compact&pageid=$pageid&index=$index&pm=$pm&session=$session&userip=&cm=$cm&uri=$uri&reqFrom=$reqfrom&remove_old=remove_old"
+	fullstring="username=$username&password=$password&provider=$provider&guestUserNameLabel=$guestusernamelabel&guestPasswordLabel=$guestpasswordlabel&passwordLabel=$passwordlabel&userNameLabel=$usernamelabel&registerGuest=$registerguest&compact=$compact&pageid=$pageid&index=$index&pm=$pm&session=$session&userip=&cm=$cm&uri=$uri&reqFrom=$reqfrom&remove_old=$remove_old"
 
 	# sending authentication
 	status=$(curl -k -s --max-time 60 --connect-timeout 30 -A "Mozilla/4.0" -d "$fullstring" https://$domain/auth/perfigo_cm_validate.jsp)
 	if [[ $(echo $status | grep "<META http-equiv='refresh' content='4;URL=http://www.google.com/'>") ]]; then
-		logger -i -s "naclogin: logged in"
+		logger -t "naclogin" -i -s "logged in"
 		exit 0
 	else
-		logger -i -s "naclogin: login failed, did you configure the settings correctly?"
+		logger -t "naclogin" -i -s "login failed, did you configure the settings correctly?"
 		exit 2
 	fi
 else
-	logger -i -s "naclogin: could not complete, are you connected to WiFi or logged in already?"
+	logger -t "naclogin" -i -s "could not complete, are you connected to WiFi or logged in already?"
 	exit 1
 fi
 exit 0
